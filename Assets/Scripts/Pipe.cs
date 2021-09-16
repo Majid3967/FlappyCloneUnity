04791a51f3897cf4ece57627a8357a068da370de
Majid <majid.ali3967@gmail.com>
2021-09-16 12:34:44 +0500

    first commit

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    private Character character;
+
+    // Use this for initialization
+    void Start()
+    {
+        character = FindObjectOfType<Character>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (character.transform.position.x - transform.position.x > 30)
+        {
+            float xRan = Random.Range(0, 10);
+            float yRan = Random.Range(-5, 5);
+
+            Instantiate(gameObject, new Vector2(character.transform.position.x + 15 + xRan, -10 + yRan), transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Vector2.right * moveSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector2.up * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Enemy")
+            Destroy(gameObject);
+    }
+}
+
